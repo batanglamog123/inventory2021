4e2b041cf840e48d83fec64a8293d016407c61ec
abc <motocapstone2021@gmail.com>
2021-06-11 09:45:46 +0800

    Added route and view for gen_report by dept

--- website/report_generation.py
+++ website/report_generation.py
@@ -1,6 +1,13 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for, jsonify, session, Response, current_app
+from flask_login import login_required, current_user
 from . import db
 from .models import Item
 from sqlalchemy import desc, asc, func
 
 report_generation = Blueprint('report_generation', __name__)
+
+@report_generation.route('/generate_dept')
+@login_required
+def generate_by_dept():
+    items = Item.query.all()
+    return render_template("generate_dept.html", items=items)
