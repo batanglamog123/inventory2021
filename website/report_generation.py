de2cd460de2a580f1cb9a8c49a19840c69b2f884
abc <motocapstone2021@gmail.com>
2021-06-11 10:40:55 +0800

    Generate report by department done!!!

--- website/report_generation.py
+++ website/report_generation.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for, jsonify, session, Response, current_app
 from flask_login import login_required, current_user
 from . import db
-from .models import Item
+from .models import Item, Department
 from sqlalchemy import desc, asc, func
 
 report_generation = Blueprint('report_generation', __name__)
@@ -9,5 +9,6 @@ report_generation = Blueprint('report_generation', __name__)
 @report_generation.route('/generate_dept')
 @login_required
 def generate_by_dept():
+    depts = Department.query.all()
     items = Item.query.all()
-    return render_template("generate_dept.html", items=items)
+    return render_template("generate_dept.html", items=items, depts=depts)
